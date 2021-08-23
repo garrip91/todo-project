c0a8a52454b17b39e74eb69c040d65ee8bd4b943
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-08-23 15:40:26 +0300

    Summary

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import TodoCompletedList, TodoListCreate, TodoRetrieveUpdateDestroy, TodoComplete
+from .views import TodoCompletedList, TodoListCreate, TodoRetrieveUpdateDestroy, TodoComplete, signup
 
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path('todos/<int:pk>/', TodoRetrieveUpdateDestroy.as_view()),
     path('todos/<int:pk>/complete/', TodoComplete.as_view()),
     path('todos/completed/', TodoCompletedList.as_view()),
+    
+    path('signup/', signup),
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -5,8 +5,33 @@ from todo.models import Todo
 
 from django.utils import timezone
 
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.parsers import JSONParser
+
+from django.http import JsonResponse
+
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+
+from rest_framework.authtoken.models import Token
+
 
 # Create your views here.
+@csrf_exempt
+def signup(request):
+    
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            user = User.objects.create_user(data['username'], password=data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse({'error': 'Данное имя пользователя уже занято. Пожалуйста, выберите новое имя пользователя!'}, status=201)
+
+
 class TodoCompletedList(generics.ListAPIView):
 
     serializer_class = TodoSerializer
